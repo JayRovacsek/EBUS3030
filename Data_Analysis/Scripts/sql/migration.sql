8f5b9f22c14f7720bdc8d2750a2bb06a6e348b72
Jay Rovacsek <jayrovacsek@gmail.com>
2018-09-01 17:57:04 +1000

    Added Price table, updated migrate script, create schema

--- Data_Analysis/Scripts/sql/migration.sql
+++ Data_Analysis/Scripts/sql/migration.sql
@@ -8,29 +8,50 @@ DROP TABLE receipt
 DROP TABLE receiptitem
 DROP TABLE staff
 */
--- Create Item table from supplied data.
-INSERT INTO Item (ItemId,ItemDescription,ItemPrice)
-SELECT DISTINCT([Item_ID]),[Item_Description],[Item_Price]
+
+-- Create Price table from supplied data.
+INSERT INTO Price(PriceId,Price)
+SELECT DISTINCT [Item_ID],[Item_Price]
 FROM [Assignment1Data]
 ORDER BY [Item_ID];
+GO
+
+-- Create Item table from supplied data.
+INSERT INTO Item (ItemId,ItemDescription)
+SELECT DISTINCT [Item_ID],[Item_Description]
+FROM [Assignment1Data]
+ORDER BY [Item_ID];
+GO
+
+-- Update Item table to include relevant PriceId
+UPDATE [Item] 
+SET [Item].[PriceId] = (
+	SELECT [Price].[PriceId]
+	FROM [Price]
+	WHERE [Price].[PriceId] = [Item].[ItemId]
+);
+GO
 
 -- Create Office table from supplied data
 INSERT INTO Office (OfficeId, OfficeLocation)
 SELECT DISTINCT([Staff_office]),[Office_Location]
 FROM [Assignment1Data]
 ORDER BY [Staff_office]
+GO
 
 -- Create Staff table from supplied data
 INSERT INTO Staff ([StaffId], [StaffFirstName],[StaffSurname],[StaffOfficeId])
 SELECT DISTINCT([Staff_ID]),[Staff_First_Name],[Staff_Surname],[Staff_office]
 FROM [Assignment1Data]
 ORDER BY [Staff_office]
+GO
 
 -- Create Customer table from supplied data
 INSERT INTO Customer ([CustomerId], [CustomerFirstName],[CustomerSurname])
 SELECT DISTINCT([Customer_ID]),[Customer_First_Name],[Customer_Surname]
 FROM [Assignment1Data]
 ORDER BY [Customer_ID]
+GO
 
 -- Nicer method to clean record 51585
 UPDATE Assignment1Data 
@@ -26282,9 +26303,12 @@ INSERT INTO Receipt([ReceiptId], [ReceiptCustomerId],[ReceiptStaffId])
 SELECT DISTINCT([Reciept_Id]),[Customer_ID],[Staff_ID]
 FROM [Assignment1Data]
 ORDER BY [Reciept_Id]
+GO
 
 -- Insert into ReceiptItem from supplied data
-INSERT INTO ReceiptItem([ReceiptId], [ItemId],[ReceiptItemQuantity])
-SELECT DISTINCT([Reciept_Id]),[Item_ID],[Item_Quantity]
-FROM [Assignment1Data]
+INSERT INTO ReceiptItem([ReceiptId], [ItemId],[ReceiptItemQuantity],[PriceId])
+SELECT DISTINCT([Reciept_Id]),[Item_ID],[Item_Quantity],[PriceId]
+FROM [Assignment1Data],[Price]
+WHERE [Price].[PriceId] = [Item_ID]
 ORDER BY [Reciept_Id]
+GO
