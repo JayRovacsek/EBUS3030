9eca02f793aab568d260e5908f1378eddf1d55a9
FullMetalEd <FullMetalEd@users.noreply.github.com>
2018-09-01 18:27:38 +1000

    total sales

---
+++ Data_Analysis/Scripts/sql/Total Sales.sql
@@ -0,0 +1,22 @@
+/* Whos the best salesman */
+
+SELECT COUNT()
+From Receipt;
+go  
+
+
+select * from receipt
+where ReceiptStaffID = 'S9'
+
+
+
+select * from staff order by StaffId asc
+
+
+SELECT StaffFirstName, StaffId, COUNT(Receipt.ReceiptStaffId) AS TotalOrderQuantity
+FROM Staff inner join Receipt on (Receipt.ReceiptStaffId = Staff.StaffId)
+group by Receipt.ReceiptStaffId, StaffFirstName, StaffId
+order by TotalOrderQuantity dsc
+go
+
+select * from receipt join receiptItem on (Receipt.ReceiptId = ReceiptItem.ReceiptId)
