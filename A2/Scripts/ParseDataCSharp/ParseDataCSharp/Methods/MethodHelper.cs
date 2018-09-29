e612e7a058d41d4c78ded80dccf265a3895765e0
Jay Rovacsek <jayrovacsek@gmail.com>
2018-09-29 13:51:31 +1000

    Fixed flaw that caused KeyError on invalidReceipt entry, ran data against A1 and determined logic works.

--- A2/Scripts/ParseDataCSharp/ParseDataCSharp/Methods/MethodHelper.cs
+++ A2/Scripts/ParseDataCSharp/ParseDataCSharp/Methods/MethodHelper.cs
@@ -37,7 +37,21 @@ namespace ParseDataCSharp.Methods
         /// <returns></returns>
         private T ParseEnum<T>(string value)
         {
-            return (T) Enum.Parse(typeof(T), Regex.Replace(value, @"\s+", ""), true);
+            return (T)Enum.Parse(typeof(T), Regex.Replace(value, @"\s+", ""), true);
+        }
+
+        private Dictionary<int, Receipt> AddToReceipts(Dictionary<int, Receipt> receipts, Receipt receipt)
+        {
+            if (receipts.ContainsKey(receipt.Id))
+            {
+                receipts.Add(receipts.Keys.LastOrDefault() + 1, receipt);
+            }
+            else
+            {
+                receipts.Add(receipt.Id, receipt);
+            }
+
+            return receipts;
         }
 
         /// <summary>
@@ -55,31 +69,31 @@ namespace ParseDataCSharp.Methods
             // Iterate over each sheet in the dataset, in our case it's only one anyway.
             foreach (DataTable table in dataSet.Tables)
                 // Iterate over each row in the dataset, it seems this iterates until it finds row with no values in any column.
-            foreach (DataRow row in table.Rows)
-            {
-                // Ignore the header of the table
-                if (counter > 0)
+                foreach (DataRow row in table.Rows)
                 {
-                    // Generation of receipt from dataset, ignoring transaction row id as it is not 
-                    // relevant and we are consolidating the duplicate items into total quantities anyway. 
-                    var receipt = new Receipt
+                    // Ignore the header of the table
+                    if (counter > 0)
                     {
-                        Id = Convert.ToInt32(row[1]),
-                        Customer = new Customer
+                        // Generation of receipt from dataset, ignoring transaction row id as it is not 
+                        // relevant and we are consolidating the duplicate items into total quantities anyway. 
+                        var receipt = new Receipt
                         {
-                            FirstName = row[3].ToString(),
-                            Id = row[2].ToString(),
-                            Surname = row[4].ToString()
-                        },
-                        Staff = new Staff
-                        {
-                            OfficeId = Convert.ToInt32(row[8]),
-                            Id = row[5].ToString(),
-                            FirstName = row[6].ToString(),
-                            Surname = row[7].ToString()
-                        },
-                        SaleDate = (DateTime) row[0],
-                        Items = new Dictionary<int, Item>
+                            Id = Convert.ToInt32(row[1]),
+                            Customer = new Customer
+                            {
+                                FirstName = row[3].ToString(),
+                                Id = row[2].ToString(),
+                                Surname = row[4].ToString()
+                            },
+                            Staff = new Staff
+                            {
+                                OfficeId = Convert.ToInt32(row[8]),
+                                Id = row[5].ToString(),
+                                FirstName = row[6].ToString(),
+                                Surname = row[7].ToString()
+                            },
+                            SaleDate = (DateTime)row[0],
+                            Items = new Dictionary<int, Item>
                         {
                             {
                                 Convert.ToInt32(row[11]), new Item
@@ -91,100 +105,101 @@ namespace ParseDataCSharp.Methods
                                 }
                             }
                         },
-                        Office = new Office
+                            Office = new Office
+                            {
+                                Id = Convert.ToInt32(row[8]),
+                                OfficeLocation = ParseEnum<Location>(row[9].ToString())
+                            }
+                        };
+
+                        // Check if receipts either is empty or doesn't have a receipt 
+                        // with the same receipt id already, if so add the receipt to 
+                        // receipts.
+                        if (receipts.Count == 0 || !receipts.ContainsKey(receipt.Id))
                         {
-                            Id = Convert.ToInt32(row[8]),
-                            OfficeLocation = ParseEnum<Location>(row[9].ToString())
+                            AddToReceipts(receipts, receipt);
+                            Console.WriteLine($@"Added new receipt: {receipt.Id}");
                         }
-                    };
-
-                    // Check if receipts either is empty or doesn't have a receipt 
-                    // with the same receipt id already, if so add the receipt to 
-                    // receipts.
-                    if (receipts.Count == 0 || !receipts.ContainsKey(receipt.Id))
-                    {
-                        receipts.Add(receipt.Id, receipt);
-                        Console.WriteLine($@"Added new receipt: {receipt.Id}");
-                    }
-                    // 
-                    else if (receipts.ContainsKey(receipt.Id))
-                    {
-                        var existingReceipt = receipts[receipt.Id];
-
-                        // Be warned; below is a fucking mess.
-
-                        // Check all staff properties match the existing receipt properties.
-                        if (existingReceipt.Staff.Id != receipt.Staff.Id
-                            || existingReceipt.Staff.FirstName != receipt.Staff.FirstName
-                            || existingReceipt.Staff.Surname != receipt.Staff.Surname)
+                        // 
+                        else if (receipts.ContainsKey(receipt.Id))
                         {
-                            invalidReceipts.Add(receipt.Id, receipt);
-                            Console.WriteLine($"Staff Id mismatch on receipt: {existingReceipt.Id}, " +
-                                              $"Staff Ids: {existingReceipt.Staff.Id} and {receipt.Staff.Id}, " +
-                                              $"Staff Names: {existingReceipt.Staff.FirstName} and {receipt.Staff.FirstName}, " +
-                                              $"Staff Surnames: {existingReceipt.Staff.Surname} and {receipt.Staff.Surname}, ");
-                            continue;
-                        }
+                            var existingReceipt = receipts[receipt.Id];
 
-                        // Check all office properties match the existing receipt properties.
-                        if (existingReceipt.Office.Id != receipt.Office.Id
-                            || existingReceipt.Office.OfficeLocation != receipt.Office.OfficeLocation)
-                        {
-                            invalidReceipts.Add(receipt.Id, receipt);
-                            Console.WriteLine($"Office Id mismatch on receipt: {existingReceipt.Id}, " +
+                            // Be warned; below is a fucking mess.
+
+                            // Check all staff properties match the existing receipt properties.
+                            if (existingReceipt.Staff.Id != receipt.Staff.Id
+                                || existingReceipt.Staff.FirstName != receipt.Staff.FirstName
+                                || existingReceipt.Staff.Surname != receipt.Staff.Surname)
+                            {
+                                AddToReceipts(invalidReceipts, receipt);
+                                Console.WriteLine($"Staff Id mismatch on receipt: {existingReceipt.Id}, " +
+                                                  $"Staff Ids: {existingReceipt.Staff.Id} and {receipt.Staff.Id}, " +
+                                                  $"Staff Names: {existingReceipt.Staff.FirstName} and {receipt.Staff.FirstName}, " +
+                                                  $"Staff Surnames: {existingReceipt.Staff.Surname} and {receipt.Staff.Surname}, ");
+                                continue;
+                            }
+
+                            // Check all office properties match the existing receipt properties.
+                            if (existingReceipt.Office.Id != receipt.Office.Id
+                                || existingReceipt.Office.OfficeLocation != receipt.Office.OfficeLocation)
+                            {
+                                AddToReceipts(invalidReceipts, receipt);
+                                Console.WriteLine($"Office Id mismatch on receipt: {existingReceipt.Id}, " +
                                               $"Office Ids: {existingReceipt.Office.Id} and {receipt.Office.Id}, " +
                                               $"Office Locations: {existingReceipt.Office.OfficeLocation} and {receipt.Office.OfficeLocation}");
-                            continue;
+                                continue;
+                            }
+
+                            // Check all date properties match the existing receipt properties.
+                            if (existingReceipt.SaleDate != receipt.SaleDate)
+                            {
+                                AddToReceipts(invalidReceipts, receipt);
+                                Console.WriteLine($"Sale date mismatch on receipt: {existingReceipt.Id}, " +
+                                                  $"Dates: {existingReceipt.SaleDate} and {receipt.SaleDate}");
+                                continue;
+                            }
+
+                            // Check all customer properties match the existing receipt properties.
+                            if (existingReceipt.Customer.Id != receipt.Customer.Id
+                                || existingReceipt.Customer.FirstName != receipt.Customer.FirstName
+                                || existingReceipt.Customer.Surname != receipt.Customer.Surname)
+                            {
+                                AddToReceipts(invalidReceipts, receipt);
+                                Console.WriteLine($"Customer mismatch on receipt: {existingReceipt.Id}, " +
+                                                  $"Customers Ids: {existingReceipt.Customer.Id} and {receipt.Customer.Id}, " +
+                                                  $"Customers Names: {existingReceipt.Customer.FirstName} and {receipt.Customer.FirstName}, " +
+                                                  $"Customers Surnames: {existingReceipt.Customer.Surname} and {receipt.Customer.Surname}, ");
+                                continue;
+                            }
+
+                            // If not item mismatches exist, we need to determine if the item 
+                            // exists in both the compared receipts.
+                            var key = receipt.Items.First().Key;
+                            var value = receipt.Items.First().Value;
+
+                            // Check if the item exists in the existing receipt already.
+                            if (existingReceipt.Items.ContainsKey(receipt.Items.First().Key))
+                            {
+                                var quantity = receipt.Items.First().Value.Quantity;
+                                Console.WriteLine($"Item exists in current receipt with Id: {key}, " +
+                                                  $"updated item quantity from: {existingReceipt.Items[key].Quantity} " +
+                                                  $"to new quantity: {existingReceipt.Items[key].Quantity + quantity}");
+
+                                // Update quantity on item in existing receipt.
+                                existingReceipt.Items[key].Quantity += quantity;
+                                receipts[existingReceipt.Id] = existingReceipt;
+                            }
+                            else
+                            {
+                                // Update the receipt with the new item added.
+                                existingReceipt.Items.Add(key, value);
+                                Console.WriteLine($@"Added item with Id: {value.Id} to existing receipt: {receipt.Id}");
+                            }
                         }
-
-                        // Check all date properties match the existing receipt properties.
-                        if (existingReceipt.SaleDate != receipt.SaleDate)
-                        {
-                            invalidReceipts.Add(receipt.Id, receipt);
-                            Console.WriteLine($"Sale date mismatch on receipt: {existingReceipt.Id}, " +
-                                              $"Dates: {existingReceipt.SaleDate} and {receipt.SaleDate}");
-                            continue;
-                        }
-
-                        // Check all customer properties match the existing receipt properties.
-                        if (existingReceipt.Customer.Id != receipt.Customer.Id
-                            || existingReceipt.Customer.FirstName != receipt.Customer.FirstName
-                            || existingReceipt.Customer.Surname != receipt.Customer.Surname)
-                        {
-                            invalidReceipts.Add(receipt.Id, receipt);
-                            Console.WriteLine($"Customer mismatch on receipt: {existingReceipt.Id}, " +
-                                              $"Customers Ids: {existingReceipt.Customer.Id} and {receipt.Customer.Id}, " +
-                                              $"Customers Names: {existingReceipt.Customer.FirstName} and {receipt.Customer.FirstName}, " +
-                                              $"Customers Surnames: {existingReceipt.Customer.Surname} and {receipt.Customer.Surname}, ");
-                            continue;
-                        }
-
-                        // If not item mismatches exist, we need to determine if the item 
-                        // exists in both the compared receipts.
-                        var key = receipt.Items.First().Key;
-                        var value = receipt.Items.First().Value;
-
-
-                        // Check if the item exists in the existing receipt already.
-                        if (existingReceipt.Items.ContainsKey(receipt.Items.First().Key))
-                        {
-                            var quantity = receipt.Items.First().Value.Quantity;
-                            Console.WriteLine($"Item exists in current receipt with Id: {key}, " +
-                                              $"updated item quantity from: {existingReceipt.Items[key].Quantity} " +
-                                              $"to new quantity: {existingReceipt.Items[key].Quantity + quantity}");
-
-                            existingReceipt.Items[key].Quantity += quantity;
-                        }
-
-                        // Update the receipt with the new item added or quantity modified.
-                        existingReceipt.Items.Add(key, value);
-                        Console.WriteLine($@"Added item with Id: {value.Id} to existing receipt: {receipt.Id}");
-                        receipts[existingReceipt.Id] = existingReceipt;
                     }
+                    counter++;
                 }
-
-                counter++;
-            }
 
             return new Tuple<Dictionary<int, Receipt>, Dictionary<int, Receipt>, int>(receipts, invalidReceipts,
                 counter);
