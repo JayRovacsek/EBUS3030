5625c2ab7e985ca7af19f902879c509fb77eb943
FullMetalEd <FullMetalEd@users.noreply.github.com>
2018-10-24 17:27:03 +1100

    added item counts for analysis

--- A2/Scripts/sql/basic_metrics_queries.sql
+++ A2/Scripts/sql/basic_metrics_queries.sql
@@ -96,8 +96,8 @@ INNER JOIN Customer c ON c.CustomerId = r.ReceiptCustomerId
 GROUP BY c.CustomerId,c.CustomerFirstName,c.CustomerSurname
 ORDER BY 'Sales Average' DESC;
 
--- Item Count, total revenue, average revenue per item sold By Office Location
-SELECT SUM(ri.ReceiptItemQuantity) AS ItemCount, o.OfficeId, o.OfficeLocation, SUM(ri.[SalePrice] * ri.[ReceiptItemQuantity]) * 0.95 as Revenue, Cast (SUM(ri.[SalePrice] * ri.[ReceiptItemQuantity]) * 0.95 as decimal)/SUM(ri.ReceiptItemQuantity) as AverageRevenue
+-- Item Count, total revenue, average revenue per item sold By Office Location no discount
+SELECT SUM(ri.ReceiptItemQuantity) AS ItemCount, o.OfficeId, o.OfficeLocation, SUM(ri.[SalePrice] * ri.[ReceiptItemQuantity]) as Revenue, Cast (SUM(ri.[SalePrice] * ri.[ReceiptItemQuantity]) as decimal)/SUM(ri.ReceiptItemQuantity) as AverageRevenue
 FROM Receipt r
 INNER JOIN ReceiptItem ri ON r.ReceiptId = ri.ReceiptId
 INNER JOIN Staff s ON s.StaffId = r.ReceiptStaffId
@@ -111,6 +111,114 @@ FROM Staff s
 INNER JOIN Office o ON o.OfficeId = s.StaffOfficeId
 Group by o.OfficeId, o.OfficeLocation
 
---3 top and bottom items
-select 
+--3 top and bottom items for office 1
+select SUM(ri.ReceiptItemQuantity) AS ItemCount, i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+from Receipt r
+INNER JOIN ReceiptItem ri ON ri.ReceiptId = r.ReceiptId
+INNER JOIN Item i on i.ItemId = ri.ItemId
+INNER JOIN Staff s on s.StaffId = r.ReceiptStaffId
+INNER JOIN Office o on o.OfficeId = s.StaffOfficeId
+where o.OfficeId = 1
+Group by i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+order by ItemCount asc
+
+--3 top and bottom items for office 2
+select SUM(ri.ReceiptItemQuantity) AS ItemCount, i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+from Receipt r
+INNER JOIN ReceiptItem ri ON ri.ReceiptId = r.ReceiptId
+INNER JOIN Item i on i.ItemId = ri.ItemId
+INNER JOIN Staff s on s.StaffId = r.ReceiptStaffId
+INNER JOIN Office o on o.OfficeId = s.StaffOfficeId
+where o.OfficeId = 2
+Group by i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+order by ItemCount asc
+
+--3 top and bottom items for office 3
+select SUM(ri.ReceiptItemQuantity) AS ItemCount, i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+from Receipt r
+INNER JOIN ReceiptItem ri ON ri.ReceiptId = r.ReceiptId
+INNER JOIN Item i on i.ItemId = ri.ItemId
+INNER JOIN Staff s on s.StaffId = r.ReceiptStaffId
+INNER JOIN Office o on o.OfficeId = s.StaffOfficeId
+where o.OfficeId = 3
+Group by i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+order by ItemCount asc
+
+--3 top and bottom items for office 4
+select SUM(ri.ReceiptItemQuantity) AS ItemCount, i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+from Receipt r
+INNER JOIN ReceiptItem ri ON ri.ReceiptId = r.ReceiptId
+INNER JOIN Item i on i.ItemId = ri.ItemId
+INNER JOIN Staff s on s.StaffId = r.ReceiptStaffId
+INNER JOIN Office o on o.OfficeId = s.StaffOfficeId
+where o.OfficeId = 4
+Group by i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+order by ItemCount asc
+
+--3 top and bottom items for office 5
+select SUM(ri.ReceiptItemQuantity) AS ItemCount, i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+from Receipt r
+INNER JOIN ReceiptItem ri ON ri.ReceiptId = r.ReceiptId
+INNER JOIN Item i on i.ItemId = ri.ItemId
+INNER JOIN Staff s on s.StaffId = r.ReceiptStaffId
+INNER JOIN Office o on o.OfficeId = s.StaffOfficeId
+where o.OfficeId = 5
+Group by i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+order by ItemCount asc
+
+--3 top and bottom items for office 6
+select SUM(ri.ReceiptItemQuantity) AS ItemCount, i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+from Receipt r
+INNER JOIN ReceiptItem ri ON ri.ReceiptId = r.ReceiptId
+INNER JOIN Item i on i.ItemId = ri.ItemId
+INNER JOIN Staff s on s.StaffId = r.ReceiptStaffId
+INNER JOIN Office o on o.OfficeId = s.StaffOfficeId
+where o.OfficeId = 6
+Group by i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+order by ItemCount asc
+
+--3 top and bottom items for office 7
+select SUM(ri.ReceiptItemQuantity) AS ItemCount, i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+from Receipt r
+INNER JOIN ReceiptItem ri ON ri.ReceiptId = r.ReceiptId
+INNER JOIN Item i on i.ItemId = ri.ItemId
+INNER JOIN Staff s on s.StaffId = r.ReceiptStaffId
+INNER JOIN Office o on o.OfficeId = s.StaffOfficeId
+where o.OfficeId = 7
+Group by i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+order by ItemCount asc
+
+--3 top and bottom items for office 8
+select SUM(ri.ReceiptItemQuantity) AS ItemCount, i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+from Receipt r
+INNER JOIN ReceiptItem ri ON ri.ReceiptId = r.ReceiptId
+INNER JOIN Item i on i.ItemId = ri.ItemId
+INNER JOIN Staff s on s.StaffId = r.ReceiptStaffId
+INNER JOIN Office o on o.OfficeId = s.StaffOfficeId
+where o.OfficeId = 8
+Group by i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+order by ItemCount asc
+
+--3 top and bottom items for office 9
+select SUM(ri.ReceiptItemQuantity) AS ItemCount, i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+from Receipt r
+INNER JOIN ReceiptItem ri ON ri.ReceiptId = r.ReceiptId
+INNER JOIN Item i on i.ItemId = ri.ItemId
+INNER JOIN Staff s on s.StaffId = r.ReceiptStaffId
+INNER JOIN Office o on o.OfficeId = s.StaffOfficeId
+where o.OfficeId = 9
+Group by i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+order by ItemCount asc
+
+--3 top and bottom items for office 10
+select SUM(ri.ReceiptItemQuantity) AS ItemCount, i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+from Receipt r
+INNER JOIN ReceiptItem ri ON ri.ReceiptId = r.ReceiptId
+INNER JOIN Item i on i.ItemId = ri.ItemId
+INNER JOIN Staff s on s.StaffId = r.ReceiptStaffId
+INNER JOIN Office o on o.OfficeId = s.StaffOfficeId
+where o.OfficeId = 10
+Group by i.ItemId, i.ItemDescription, o.OfficeId, o.OfficeLocation
+order by ItemCount asc
+
 
